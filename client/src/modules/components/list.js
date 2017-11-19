2ade50687a7b6503fd718b3b71acb18dfff7bf88
rmbaitinger44 <rmbaitinger@gmail.com>
2017-11-19 15:12:49 -0600

    d

--- client/src/modules/components/list.js
+++ client/src/modules/components/list.js
@@ -7,7 +7,21 @@ export class List {
   constructor(router) {
 	        this.router = router;
           this.message = 'List';
+          this.auth = auth;
+          
+          this.user = JSON.parse(sessionStorage.getItem('user'));
+          this.showList = true;
   }
+  createTodo(){	
+		this.todoObj = {
+			todo: "",
+			description: "",
+			dateDue: new Date(),
+			 userId: this.user._id,
+			priority: this.priorities[0]
+		}
+		this.showList = false;		
+	}
 
   logout(){
     sessionStorage.removeItem('user');
---
+++ client/src/resources/data/todos.js
@@ -0,0 +1,17 @@
+import {inject} from 'aurelia-framework';
+import {DataServices} from './data-services';
+
+@inject(DataServices)
+export class ToDos {
+	constructor(data) {
+        		this.data = data;
+        		this.TODO_SERVICE = 'todos';
+   		 }
+}
+async save(todo){
+        if(todo){
+            let serverResponse = await this.data.post(user, this.TODO_SERVICE);
+            return serverResponse;
+        }
+    }
+    
