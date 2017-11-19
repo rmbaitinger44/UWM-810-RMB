3e5fbabf1468f710149e7db90e9592bf9932d409
rmbaitinger44 <rmbaitinger@gmail.com>
2017-11-19 15:48:20 -0600

    d

--- client/src/modules/components/list.js
+++ client/src/modules/components/list.js
@@ -1,9 +1,16 @@
 import {inject} from 'aurelia-framework';
+import {ToDos} from '../resources/data/todos';
+import { AuthService } from 'aurelia-auth';
+
 import {Router} from 'aurelia-router';
 
-@inject(Router)
+
+
+inject(Router)
 export class List {
 
+  
+  
   constructor(router) {
 	        this.router = router;
           this.message = 'List';
@@ -21,7 +28,19 @@ export class List {
 			priority: this.priorities[0]
 		}
 		this.showList = false;		
+  }
+  async saveTodo(){
+		if(this.todoObj){		
+			let response = await this.todos.save(this.todoObj);
+			if(response.error){
+				alert("There was an error creating the ToDo");
+			} else {
+				//Could provide feeback									
+			}
+			this.showList = true;
+		}
 	}
+
 
   logout(){
     sessionStorage.removeItem('user');
